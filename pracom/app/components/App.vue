da4fbb20d11455ba9f3da29aa85712f7d7d518d0
YujinHyeon <gusdbwls9772@gmail.com>
2019-12-30 18:04:41 +0900

    practice2

--- pracom/app/components/App.vue
+++ pracom/app/components/App.vue
@@ -3,17 +3,29 @@
     <ActionBar title="Master" />
     <StackLayout>
       <Button text="To Details directly" @tap="goToDetailPage" class="nextBtn" />
+      <Label text="hello world" @tap="goToDetailPage2" class="helloworld" />
+      <AbsoluteLayout backgroundColor="#3c495e">
+        <Label text="10,10" left="10" top="10" width="100" height="100" backgroundColor="#43b883"/>
+        <Label text="120,10" left="120" top="10" width="100" height="100" backgroundColor="#43b883"/>
+        <Label text="10,120" left="10" top="120" width="100" height="100" backgroundColor="#43b883"/>
+        <Label text="120,120" left="120" top="120" width="100" height="100" backgroundColor="#43b883"/>
+      </AbsoluteLayout>
     </StackLayout>
   </Page>
 </template>
 
 <script >
 import Detail from './Detail.vue';
+import Detail2 from './Detail2.vue';
 
 export default {
   methods: {
     goToDetailPage() {
       this.$navigateTo(Detail);
+    },
+
+    goToDetailPage2() {
+      this.$navigateTo(Detail2);
     },
 
     // Detail Page를 Modal Page로 보여주고 싶은 경우
@@ -44,5 +56,11 @@ export default {
         font-size: 30rem;
         height: 200px;
         font-weight: bold;
+    }
+
+    .helloworld {
+      background: #333333;
+      text-align: center;
+      color: #FFF;
     }
 </style>
