087bcc8c7edce8f99e18e184faec61115ab0512e
YujinHyeon <gusdbwls9772@gmail.com>
2019-12-30 17:33:37 +0900

    Practice Router

---
+++ pracom/app/components/Detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <Page>
+    <ActionBar title="Detail" />
+    <StackLayout>
+      <Label text="Detail Page" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
