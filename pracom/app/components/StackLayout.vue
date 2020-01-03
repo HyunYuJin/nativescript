ebd1939e8deb59146f6fd1fce0e99cf8a9a92676
unknown <gusdbwls9772@gmail.com>
2020-01-03 14:17:52 +0900

    Add StackLayout

--- pracom/app/components/StackLayout.vue
+++ pracom/app/components/StackLayout.vue
@@ -7,10 +7,17 @@
             <Label text="third" height="70" backgroundColor="#1C6B48" />
             <Button text="Back To Master" width="300" height="50" @tap="$navigateBack" />
 
-            <StackLayout height="300" orientation="horizontal" backgroundColor="#DDD">
+            <StackLayout height="200" orientation="horizontal" backgroundColor="#DDD">
                 <Label text="first" width="70" backgroundColor="skyblue" />
                 <Label text="second" width="70" backgroundColor="pink" />
                 <Label text="third" width="70" backgroundColor="purple" />
+            </StackLayout>
+
+            <StackLayout>
+                <Label text="left" horizontalAlignment="left" width="33%" height="70" backgroundColor="#43B883" />
+                <Label text="center" horizontalAlignment="center" width="33%" height="70" backgroundColor="#289062" />
+                <Label text="right" horizontalAlignment="right" width="33%" height="70" backgroundColor="#1C6B48" />
+                <Label text="stretch" horizontalAlignment="stretch" height="70" backgroundColor="#43b883"/>
             </StackLayout>
         </StackLayout>
     </Page>
